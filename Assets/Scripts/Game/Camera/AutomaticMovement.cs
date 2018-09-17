3b134d4304082c48d28b8f40595ce2ca2d617e8f
Michael Vinicio Rodríguez <mvrd_17@hotmail.com>
2018-09-16 19:20:26 -0600

    Basic player behaviour

---
+++ Assets/Scripts/Game/Camera/AutomaticMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticMovement : MonoBehaviour {
+	public GameObject target; //Public variable to store a reference to the player game object
+	private Rigidbody targetRB;
+
+	public float smoothSpeed = 0.125f;
+	public Vector3 offset;
+
+	public bool perspective = false;
+	// Use this for initialization
+	void Start () { }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 desirePosition = target.transform.position + offset;
+		Vector3 smoothedPosition = Vector3.Lerp (transform.position, desirePosition, smoothSpeed);
+		transform.position = smoothedPosition;
+		if (perspective)
+			transform.LookAt (target.transform);
+	}
+}
---
+++ Assets/Scripts/Game/Player/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 1000.0f;
+	Rigidbody m_Rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		m_Rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		// Make it move 10 meters per second instead of 10 meters per frame...
+		float translation = Input.GetAxis ("Horizontal") * speed;
+		float translationUp = Input.GetAxis ("Vertical") * speed;
+		translation *= Time.deltaTime;
+		translationUp *= Time.deltaTime;
+
+		// Get the horizontal and vertical axis.
+		// By default they are mapped to the arrow keys.
+		// The value is in the range -1 to 1
+		// float translation = Input.GetAxis ("Vertical") * speed;
+
+		// Move translation along the object's z-axis
+		// transform.Translate (translation, 0, 0);
+		Vector3 v3 = new Vector3(translation, translationUp, 0); 
+		m_Rigidbody.AddForce(v3);
+
+		// transform.Translate (0, translationUp, 0);
+	}
+}
