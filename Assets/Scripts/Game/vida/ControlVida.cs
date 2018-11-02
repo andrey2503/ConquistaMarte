37d55ef16066f680f7f223e3f8925a5959a88623
andrey2503 <andreytorresvega@gmail.com>
2018-11-02 15:33:29 -0600

    Barra de vida

--- Assets/Scripts/Game/vida/ControlVida.cs
+++ Assets/Scripts/Game/vida/ControlVida.cs
@@ -5,9 +5,11 @@ using UnityEngine.UI;
 public class ControlVida : MonoBehaviour {
 	public Text vida;
 	int cantidadVida=100;
+	public Scrollbar barraVida;
 	// Use this for initialization
 	void Start () {
 		vida.text= "" + cantidadVida;
+		barraVida.size = cantidadVida;
 	}
 	
 	void OnCollisionEnter(Collision cinfo){
@@ -15,22 +17,32 @@ public class ControlVida : MonoBehaviour {
 		if(cinfo.relativeVelocity.magnitude > 5){
 			cantidadVida -= 5;
 			vida.text= "" + cantidadVida;
+			barraVida.size = cantidadVida/100f;
+
 		}//
 		if(cinfo.relativeVelocity.magnitude > 15){
 			cantidadVida -= 5;
 			vida.text= "" + cantidadVida;
+			barraVida.size = cantidadVida/100f;
+
 		}// 
 		if(cinfo.relativeVelocity.magnitude > 25){
 			cantidadVida -= 5;
 			vida.text= "" + cantidadVida;
+			barraVida.size = cantidadVida/100f;
+
 		}// 
 		if(cinfo.relativeVelocity.magnitude > 35){
 			cantidadVida -= 5;
 			vida.text= "" + cantidadVida;
+			barraVida.size = cantidadVida/100f;
+
 		}// 
 		if(cinfo.relativeVelocity.magnitude > 50){
 			cantidadVida -= 5;
 			vida.text= "" + cantidadVida;
+			barraVida.size = cantidadVida/100f;
+
 		}// 
 		if(cantidadVida <= 0){
 			ControlMenus.instance.gameOver ();
