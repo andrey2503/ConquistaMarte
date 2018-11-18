a24e3853c4c2d109697695cf50ba8daaf0db3777
Michael <mvrd_17@hotmail.com>
2018-11-18 00:31:54 -0600

    Narrativa

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+	public AudioSource audiosource;
+	public int delay = 0;
+
+    void Start()
+    {
+        AudioSource audiosource = GetComponent<AudioSource>();
+		
+		someOtherMethodInAMonoBehaviour();
+    }
+
+    IEnumerator playSoundAfterTenSeconds()
+    {
+        yield return new WaitForSeconds(delay);
+        audiosource.Play();
+    }
+
+    // then elsewhere when you want to invoke it:
+
+    void someOtherMethodInAMonoBehaviour()
+    {
+        StartCoroutine(playSoundAfterTenSeconds());
+    }
+
+}
