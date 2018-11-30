aa2931149ede301c5479ea368ca771f6656f70e5
Michael <mvrd_17@hotmail.com>
2018-11-29 23:36:29 -0600

    Final scene

---
+++ Assets/Scripts/Game/EscenaFinal/FinalManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(goHome());
+    }
+
+    // Update is called once per frame
+    private IEnumerator goHome() {
+		yield return new WaitForSeconds (50);
+		SceneManager.LoadScene ("Pantalla1");
+	}
+}
